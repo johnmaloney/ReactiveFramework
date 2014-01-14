bf3b485535cc93e97897e4cb89f49f786cf91eb5
johnmaloney <maloney1@gmail.com>
2014-01-14 09:29:48 -0700

    Attempting to get index page by default

--- Aetos.Messaging.Clients.Web/App_Start/RouteConfig.cs
+++ Aetos.Messaging.Clients.Web/App_Start/RouteConfig.cs
@@ -15,8 +15,7 @@ namespace Aetos.Messaging.Clients.Web
 
             routes.MapRoute(
                 name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                url: "/index.html"
             );
         }
     }
