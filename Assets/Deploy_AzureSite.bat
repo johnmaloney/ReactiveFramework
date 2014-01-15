cccb6ded575af756cd9de83849828ff8abc87d32
johnmaloney <maloney1@gmail.com>
2014-01-15 16:52:26 -0700

    Altering Deployment setup

--- Assets/Deploy_AzureSite.bat
+++ Assets/Deploy_AzureSite.bat
@@ -1,3 +1,7 @@
 rem https://aetosDeploy@aetosmessaging.scm.azurewebsites.net:443/AetosMessaging.git
 
+rem FIRST RUN THIS:
+git remote add snlgis https://aetosDeploy@snlgis.scm.azurewebsites.net:443/snlgis.git
+
+rem TO DEPLOY RUN THIS:
 run cmd C:\Users\maloney1\Source\Repos\ReactiveFramework>git push azure 0.1
