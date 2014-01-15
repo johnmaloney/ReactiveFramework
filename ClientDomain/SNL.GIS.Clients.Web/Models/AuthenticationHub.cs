069a89999175419c836cf6818bf2e437cb332b01
johnmaloney <maloney1@gmail.com>
2014-01-15 16:56:45 -0700

    Authentication Hub addition

---
+++ ClientDomain/SNL.GIS.Clients.Web/Models/AuthenticationHub.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using Aetos.Messaging.Domain;
+using Aetos.Messaging.Domain.Clients;
+using Aetos.Messaging.Interfaces;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+using Newtonsoft.Json;
+using SNL.GIS.Messaging.Domain;
+using SNL.GIS.Messaging.Domain.Commands;
+using SNL.GIS.Messaging.Domain.Events;
+
+namespace SNL.GIS.Clients.Web.Models
+{
+    [HubName("authenticationHub")]
+    public class AuthenticationHub : Hub
+    {
+        #region Fields
+
+        private IQueueClient queueClient = new QueueClient(SNLQueue.AuthenticateUserCommand);
+
+        #endregion
+
+        #region Methods
+        
+        public void Login(string identifier)
+        {
+            var message = new Message()
+            {
+                Body = new AuthenticateUserCommand()
+                {
+                    InstanceId = Guid.NewGuid(),
+                    Identifier = Context.ConnectionId
+                }
+            };
+
+            queueClient.Send(message);
+        }
+
+        #endregion
+    }
+
+    public class AuthenticationSubscriber
+    {
+        #region Fields
+
+        private TopicClient topicClient;
+
+        #endregion
+
+        #region Properties
+
+        public static AuthenticationSubscriber Instance { get; set; }
+        private IHubConnectionContext Clients { get; set; }
+
+        #endregion
+
+        #region Methods
+
+        private AuthenticationSubscriber()
+        {
+            Clients = GlobalHost.ConnectionManager.GetHubContext<AuthenticationHub>().Clients;
+            var subscriberName = Dns.GetHostName();
+            topicClient = new TopicClient(SNLTopic.UserAuthenticatedEvent, subscriberName);
+            topicClient.Subscribe(OnMessageReceived);
+        }
+
+        private void OnMessageReceived(Message message)
+        {
+            var authEvent = message.Body as UserAuthenticatedEvent;
+
+            var json = JsonConvert.SerializeObject(authEvent);
+            Clients.Client(authEvent.Identifier).authenticated(json);
+        }
+
+        public static void Start()
+        {
+            if (Instance == null)
+                Instance = new AuthenticationSubscriber();
+        }
+
+        public static void Stop()
+        {
+            if (Instance != null)
+            {
+                Instance.StopInternal();
+                Instance = null;
+            }
+        }
+
+        private void StopInternal()
+        {
+            topicClient.DeleteSubscription();
+            topicClient.Unsubscribe();
+            topicClient = null;
+        }
+
+        #endregion
+    }
+}
---
+++ ClientDomain/SNL.GIS.Clients.Web/Scripts/app/messaging.js
@@ -0,0 +1,38 @@
+﻿$(function () {
+
+
+
+    //function updateMessageEvent(msg) {
+    //    var newMessage = JSON.parse(msg);
+    //    var messages = $('#messages');
+    //    var messagesBody = messages.find('tbody');
+
+    //    var newRow = '<tr> ' +
+    //                    '<td>' + newMessage.LastUpdated + '</td>' +
+    //                    '<td>' + newMessage.Message + '</td>' +
+    //                    '<td>' + newMessage.PublishedBy + '</td>' +
+    //                    '<td>' + newMessage.TransportedBy + '</td>' +
+    //                 '</tr>'
+
+    //    messagesBody.append(newRow);
+    //};
+
+    //hub.client.newMessageEvent = function (msg) {
+    //    updateMessageEvent(msg);
+    //};
+
+    //var messages = $('#messages');
+    //var messagesBody = messages.find('tbody');
+    //messagesBody.empty();
+    //messagesBody.append('<tr><td colspan="4">Loading.......</td></tr>');
+
+    $.connection.hub.start();
+    
+    var userAuthentication = new UserAuthentication($.connection.authenticationHub);
+    $('#authenticateUser').click(function () {
+        userAuthentication.login();
+    });
+
+
+    
+});
